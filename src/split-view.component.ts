167b038dca65798d6647961b9a53e02297f2b4d4
quinntynebrown@gmail.com <quinntynebrown@gmail.com>
2018-11-15 16:33:54 -0500

    mutiple layouts

--- src/split-view.component.ts
+++ src/split-view.component.ts
@@ -3,6 +3,11 @@ import { unsafeHTML } from "lit-html/directives/unsafe-html";
 
 const styles = unsafeHTML(`<style>${require("./split-view.component.css")}<style>`);
 
+export const enum SplitViewLayout {
+  vertical, 
+  horizontal
+}
+
 export class SplitViewComponent extends HTMLElement {
   
   constructor() {
@@ -18,35 +23,63 @@ export class SplitViewComponent extends HTMLElement {
     if (!this.hasAttribute('role'))
       this.setAttribute('role', 'splitview');
 
-    this.style.setProperty("--grid-left-column-width",`${this.leftPanelWidth}px`); 
+    
+      if(this.classList.contains("vertical"))
+        this.style.setProperty("--grid-main-panel-width",`${this.mainPanelWidth}px`);
+      
+      if(this.classList.contains("horizontal"))
+        this.style.setProperty("--grid-main-panel-height",`${this.mainPanelHeight}px`);
   }
 
   sashCurrentx:number;
 
-  get leftPanelWidth() {
-    return +(<HTMLElement>this.children[1]).offsetWidth;
+  sashCurrenty:number;
+
+  get layout(): SplitViewLayout { 
+    return this.classList.contains("vertical") 
+    ? SplitViewLayout.vertical 
+    : SplitViewLayout.horizontal; 
   }
+
+  get mainPanel():HTMLElement { return <HTMLElement>this.querySelector('[slot=main]'); }
+
+  get mainPanelWidth(): number { return +this.mainPanel.offsetWidth; }
+
+  get mainPanelHeight():number { return +this.mainPanel.offsetHeight; } 
 
   get template(): TemplateResult {
     return html`
       ${styles}
-      <slot name='left'></slot>
+      <slot name='main'></slot>
       <ce-sash @didStart=${this.onSashDidStart} 
         @didChange=${this.onSashDidChange}></ce-sash>
-      <slot name='right'></slot>
+      <slot name='secondary'></slot>
     `;
   }
 
   onSashDidStart(e:CustomEvent) {    
     this.sashCurrentx = +e.detail["startx"];
+    this.sashCurrenty = +e.detail["starty"];
   }
 
   onSashDidChange(e:CustomEvent) {
-    const change = this.sashCurrentx - (+e.detail["currentx"]);
-    
-    this.style.setProperty("--grid-left-column-width",`${this.leftPanelWidth - change}px`);
 
-    this.sashCurrentx = (+e.detail["currentx"]);
+    const currentx = +e.detail["currentx"];
+    const currenty = +e.detail["currenty"];
+    
+    console.log(`${this.mainPanelWidth - this.sashCurrentx - currentx}px`);
+
+    if(this.layout == SplitViewLayout.vertical) {
+      this.style.setProperty("--grid-main-panel-width",`${this.mainPanelWidth - this.sashCurrentx + currentx}px`);
+    }
+    
+    if(this.layout == SplitViewLayout.horizontal) {       
+      this.style.setProperty("--grid-main-panel-height",`${this.mainPanelHeight - this.sashCurrenty + currenty}px`);
+    }
+
+    this.sashCurrentx = currentx;
+
+    this.sashCurrenty = currenty;
   }
 }
 
